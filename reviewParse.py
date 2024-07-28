3665a668e9c1c79b9cf02fcb8ebc816922f87f5d
Aidan Gooding <92827902+WhyPine@users.noreply.github.com>
2024-07-27 22:46:26 -0700

    Add files via upload

--- reviewParse.py
+++ reviewParse.py
@@ -13,6 +13,22 @@ DB_USER = "postgres"
 DB_PASS = "ADMIN"
 DB_HOST = "localhost"
 
+asd = "host=cranalytics.postgres.database.azure.com;dbname=postgres;port=5432;user=psqlsde342nzxc01243x34568zx;password=sbe3234k.1234bhjcbjyfgh345!-szv678r137/?das1;Ssl Mode=Require"
+
+ENV_VARS = asd
+
+split = ENV_VARS.split(';')
+PARAMS = {}
+for pair in split:
+    key,value = pair.split('=')
+    PARAMS[key] = value
+
+
+DB_USER=PARAMS['user']
+DB_PASS=PARAMS['password']
+DB_HOST=PARAMS['host']
+DB_NAME=PARAMS['dbname']
+
 # Connect to the PostgreSQL database
 conn = psycopg2.connect(
     dbname=DB_NAME,
@@ -25,13 +41,34 @@ cursor = conn.cursor()
 client = CrunchyrollClient("goodingaidan@gmail.com",  "Sx/6@SBzk&.ghKP")
 client.auth()
 
+cursor.execute(
+"""
+CREATE TABLE reviews (
+    series_id VARCHAR(10),
+    review_id UUID PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    language VARCHAR(10) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    modified_at TIMESTAMP NOT NULL,
+    authored_reviews INT NOT NULL,
+    spoiler BOOLEAN NOT NULL,
+    author_rating VARCHAR(5) NOT NULL,
+    author JSONB NOT NULL,
+    ratings JSONB NOT NULL,
+    reported BOOLEAN NOT NULL
+);
+"""
+)
 # Read the CSV file and insert data into the database
 cursor.execute("SELECT linked_resource_key FROM shows;")
 shows = cursor.fetchall()
 for show in shows:
     seriesID = show[0].split("/")[2]
-    reviews = methods.listSeriesReviews(client, seriesID, page_size=20)
+    reviews = methods.listSeriesReviews(client, seriesID, page_size=50)
     print("---------")
+    if not reviews:
+        continue
     for review in reviews:
         #print(row['series_metadata'].replace("'", "\""))
         cursor.execute(
